eb606b9c6cf26347fe77ac8f86008fed045d49a9
langstonBT <benlangston77@gmail.com>
2024-01-19 22:06:34 +0000

    Reset to it was before

--- src/Componants/Navbar.js
+++ src/Componants/Navbar.js
@@ -6,8 +6,6 @@ const Navbar = () => {
       <NavLink to="/"className="home-logo-link">#VANLIFE</NavLink>
       <NavLink to="/about"className="about-link">About</NavLink>
       <NavLink to="/vans"className="vans-link">Vans</NavLink>
-      <h1>hello</h1>
-      <h1>This is a change i have just made as of 19/01/2024</h1>
     </nav>
   )
 }
